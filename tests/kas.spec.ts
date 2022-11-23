8fa7ea26df393856567aa114b1ecb8b9ae879420
Adrián Sanz Gómiz <100594859+asanzgom@users.noreply.github.com>
2022-11-23 09:59:07 +0100

    Update kas.spec.ts

--- tests/kas.spec.ts
+++ tests/kas.spec.ts
@@ -80,19 +80,19 @@ test('test Kafka list filtered by status', async ({ page }) => {
   await expect(page.getByText('No results found')).toHaveCount(1);
 
   await filterByStatus(page, 'Creating');
-  expect(page.getByText(testInstanceName)).toBeTruthy();
+  await expect(page.getByText(testInstanceName)).toBeTruthy();
 
   // Reset the filter
   await resetFilter(page);
   await waitForKafkaReady(page, testInstanceName);
 
   await filterByStatus(page, 'Ready');
-  expect(page.getByText(testInstanceName)).toBeTruthy();
+  await expect(page.getByText(testInstanceName)).toBeTruthy();
 
   await deleteKafkaInstance(page, testInstanceName, false);
 
   await filterByStatus(page, 'Deleting');
-  expect(page.getByText(testInstanceName)).toBeTruthy();
+  await expect(page.getByText(testInstanceName)).toBeTruthy();
 
   // await for the kafka instance to be deleted
   await expect(page.getByText(`${testInstanceName}`, { exact: true })).toHaveCount(0, {
