a463b0603c6ccb1ad665f183aca528edf253651d
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-11-25 15:14:25 +0000

    eslint plawright plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:playwright/playwright-test'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
