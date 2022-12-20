c0649298a23ad583af2c0e235b7907ce9ac527d2
Jakub Stejskal <xstejs24@gmail.com>
2022-12-20 10:14:48 +0100

    Fix lint
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- tests/kas_messaging.spec.ts
+++ tests/kas_messaging.spec.ts
@@ -25,6 +25,7 @@ let credentials;
 let bootstrap: string;
 
 test.beforeEach(async ({ page }) => {
+  // @ts-ignore
   test.skip(config.startingPage === config.startingPageDefault);
 
   await login(page);
