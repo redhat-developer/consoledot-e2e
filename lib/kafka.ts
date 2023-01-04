ce9acd04de59fa904478c688ed8d54c63c82611a
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-01-04 16:18:42 +0000

    Revert "Add sleep method and use it in grant permissions" (#60)

--- lib/kafka.ts
+++ lib/kafka.ts
@@ -1,7 +1,6 @@
 import { expect, Page } from '@playwright/test';
 import { config } from './config';
 import { closePopUp } from './popup';
-import { sleep } from './common';
 
 export const navigateToApplicationAndDataServices = async function (page: Page) {
   if (!(await page.locator('button', { hasText: 'Streams for Apache Kafka' }).isVisible())) {
@@ -120,8 +119,6 @@ export const grantProducerAccess = async function (page: Page, saId: string, top
   await page.getByPlaceholder('Enter prefix').click();
 
   await page.getByRole('button').filter({ hasText: 'Save' }).click();
-  //Sleep 5s to propagate changes
-  await sleep(5000);
 };
 
 // TODO - we shouldn't use just prefix for topic/group but also complete name
@@ -164,8 +161,6 @@ export const grantConsumerAccess = async function (page: Page, saId: string, top
     .fill(consumerGroup);
 
   await page.getByRole('button').filter({ hasText: 'Save' }).click();
-  //Sleep 5s to propagate changes
-  await sleep(5000);
 };
 
 export const navigateToAccess = async function (page: Page, kafkaName: string) {
