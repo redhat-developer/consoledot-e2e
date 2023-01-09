935239b6b8c0b1b5606ab0f8ad563f283c3cd8dd
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-01-09 10:42:53 +0000

    More strict retry params

--- tests/kas_messaging.spec.ts
+++ tests/kas_messaging.spec.ts
@@ -22,8 +22,8 @@ const testServiceAccountName = 'test-service-account';
 const testMessageKey = 'key';
 const consumerGroupId = 'test-consumer-group';
 const expectedMessageCount = 100;
-const reconnectCount = 10;
-const reconnectDelayMs = 1000;
+const reconnectCount = 5;
+const reconnectDelayMs = 500;
 let credentials;
 let bootstrap: string;
 
