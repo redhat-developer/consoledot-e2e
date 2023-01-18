89a8247a19b5952de47c591a2385ded769277a78
David Kornel <kornys@outlook.com>
2023-01-18 11:13:57 +0100

    Enable testing on multiple web browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -56,19 +56,21 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
-    //
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1920, height: 1024 },
+      },
+    },
+
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        viewport: { width: 1920, height: 1024 },
+      },
+    },
 
     /* Test against mobile viewports. */
     // {
