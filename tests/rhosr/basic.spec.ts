d09ddc0312ee42180c9c5fe06b443e53fffa8cb6
rkubis <rkubis@redhat.com>
2023-01-26 09:56:29 +0100

    New(tests): Basic tests (login, logout) for RHOSR (#85)
    * New(tests): Basic tests for RHOSR
    
    * Fix(formatting): Use Prettier format
    
    * Remove(test): Duplicate test for login+logout from RHOSR set
    
    * Remove(import): Unused logout

---
+++ tests/rhosr/basic.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import login from '@lib/auth';
+
+test('perform login', async ({ page }) => {
+  await login(page);
+
+  // navigate to Service Registry
+  await page.locator('#chr-c-sidebar').getByText('Application and Data Services').click();
+  await page
+    .getByRole('heading', {
+      name: 'Red Hat OpenShift Service Registry'
+    })
+    .click();
+  await page.getByRole('button', { name: 'Service Registry' }).click();
+  await page
+    .getByRole('region', { name: 'Service Registry' })
+    .getByRole('link', { name: 'Service Registry Instances' })
+    .click();
+
+  await expect(page).toHaveTitle(/Service Registry | Red Hat OpenShift Application Services/);
+  await expect(page.getByRole('heading', { name: /Service Registry Instances/ })).toBeVisible();
+});
