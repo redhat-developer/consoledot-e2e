af8cb8b14b2241da16a92ed1b65bad76d4cb6cfd
Adrián Sanz Gómiz <100594859+asanzgom@users.noreply.github.com>
2023-02-07 12:28:41 +0100

    NEW: Test for custom Topic properties different than default (#99)

--- lib/topic.ts
+++ lib/topic.ts
@@ -1,25 +1,61 @@
 import { expect, Page } from '@playwright/test';
-import { showElementActions, waitForKafkaReady } from './kafka';
+import { showElementActions } from './kafka';
 import { navigateToKafkaList } from './navigation';
 
 export const navigateToKafkaTopicsList = async function (page: Page, kafkaName: string) {
   await expect(page.getByText(kafkaName)).toHaveCount(1);
-  await waitForKafkaReady(page, kafkaName);
   await page.locator('a', { hasText: kafkaName }).click();
   await expect(await page.locator('button[aria-label="Topics"]')).toHaveCount(1);
   // data-testid=pageKafka-tabTopics
   await page.locator('button[aria-label="Topics"]').click();
 };
 
-export const createKafkaTopic = async function (page: Page, name: string) {
+export const createKafkaTopic = async function (page: Page, name: string, defaultProperties: boolean) {
   await page.locator('button', { hasText: 'Create topic' }).click();
   await expect(page.getByText('Create topic')).toHaveCount(2);
-  // This is default Topic creation
   await page.getByPlaceholder('Enter topic name').fill(name);
-  for (let i = 0; i < 3; i++) {
-    await page.locator('button', { hasText: 'Next' }).click();
+  if (defaultProperties) {
+    // This is default properties values Topic creation
+    for (let i = 0; i < 3; i++) {
+      await page.locator('button', { hasText: 'Next' }).click();
+    }
+    await page.locator('button', { hasText: 'Finish' }).click();
+  } else {
+    // Use different values
+    await page.locator('label:has-text("Show all available optionsShow all available options") span').first().click();
+
+    await expect(page.locator('input[name="num-partitions"]')).toHaveValue('1', { timeout: 3000 });
+    // Increasing twice and decreasing once the num of partitions to test + & -
+    for (let i = 0; i < 2; i++) {
+      await page.locator('button[name="num-partitions"]').nth(1).click();
+    }
+    await page.locator('button[name="num-partitions"]').nth(0).click();
+
+    await expect(page.locator('label:has-text("days") input[type="number"]')).toHaveCount(1);
+    // Increasing twice and decreasing once the units for Retention Time to test + & -
+    for (let i = 0; i < 2; i++) {
+      await page.locator('button[name="retention-ms"]').nth(1).click();
+    }
+    await page.locator('button[name="retention-ms"]').nth(0).click();
+    await page.locator('button:has-text("days")').click();
+    await page.locator('button', { hasText: 'hours' }).click();
+
+    await page.getByLabel('bytes').check();
+    // Increasing twice and decreasing once the units for Retention Time to test + & -
+    for (let i = 0; i < 2; i++) {
+      await page.locator('button[name="retention-bytes"]').nth(1).click();
+    }
+    await page.locator('button[name="retention-bytes"]').nth(0).click();
+    await page.locator('button:has-text("bytes")').click();
+    await page.locator('button', { hasText: 'kibibytes' }).click();
+
+    // Choosing different CleanUp policy
+    await page.locator('button:has-text("Delete")').click();
+    await page.getByText('Compact').first().click();
+    await page.locator('button:has-text("Compact")').click();
+
+    await page.locator('button', { hasText: 'Create topic' }).click();
   }
-  await page.locator('button', { hasText: 'Finish' }).click();
   await expect(page.getByText(name)).toHaveCount(1);
 };
 
@@ -38,12 +74,18 @@ export const navigateToMessages = async function (page: Page, kafkaName: string,
   await navigateToKafkaList(page);
   await navigateToKafkaTopicsList(page, kafkaName);
   await expect(await page.locator('a', { hasText: topicName })).toHaveCount(1);
-
   await page.locator('a', { hasText: topicName }).click();
-
   await expect(await page.locator('button', { hasText: 'Messages' })).toHaveCount(1);
-
   await page.locator('button', { hasText: 'Messages' }).click();
+};
+
+export const navigateToProperties = async function (page: Page, kafkaName: string, topicName: string) {
+  await navigateToKafkaList(page);
+  await navigateToKafkaTopicsList(page, kafkaName);
+  await expect(await page.locator('a', { hasText: `${topicName}` })).toHaveCount(1);
+  await page.locator('a', { hasText: `${topicName}` }).click();
+  await expect(page.locator('h1:has-text("' + `${topicName}` + '")')).toHaveCount(1);
+  await page.getByTestId('pageTopic-tabProperties').click();
 };
 
 export const refreshMessages = async function (page: Page) {
