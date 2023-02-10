aa33c4a5c23e6ab7f75491000fe78f1d2bc214e8
Adrián Sanz Gómiz <100594859+asanzgom@users.noreply.github.com>
2023-02-10 11:28:54 +0100

    temporarily disable retries (#106)

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,8 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* retries: process.env.CI ? 2 : 0, */
+  retries: 0,
   /* Opt out of parallel tests. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
