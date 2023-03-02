34582b91b3fb760ae51b68a432adb3bfeb348552
Jakub Stejskal <xstejs24@gmail.com>
2023-03-02 18:09:10 +0100

    Use analytics blocker in fixture
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- lib/utils/fixtures.ts
+++ lib/utils/fixtures.ts
@@ -1,10 +1,12 @@
 import { config } from '@lib/config';
 import { test as base } from '@playwright/test';
+import blockAnalyticsDomains from '@lib/utils/blocker';
 
 // Extend Playright page to start always at config.startingPage
 export const test = base.extend({
   page: async ({ page }, use) => {
     await page.goto(config.startingPage);
+    await blockAnalyticsDomains(page);
     await use(page);
   }
 });
