fb2f478a2f3ad418dd9f4cb98879079277025f45
David Kornel <kornys@outlook.com>
2023-03-09 20:11:50 +0100

    Log console errors to output when test fail

--- lib/utils/fixtures.ts
+++ lib/utils/fixtures.ts
@@ -4,12 +4,33 @@ import blockAnalyticsDomains from '@lib/utils/blocker';
 
 // Extend Playright page to start always at config.startingPage
 export const test = base.extend({
-  page: async ({ page }, use) => {
+  page: async ({ page }, use, testInfo) => {
+    const errLogs = [];
     await blockAnalyticsDomains(page);
+
+    if (config.enableErrLogging) {
+      page.on('console', (msg) => {
+        if (msg.type() === 'error') errLogs.push(`Error: "${msg.text()}"`);
+      });
+
+      page.on('pageerror', (err) => {
+        errLogs.push(`PageError: ${err.message}`);
+      });
+
+      page.on('requestfailed', (request) => {
+        errLogs.push(`Request failed: ${request.url()} ${request.failure().errorText}`);
+      });
+    }
     await page.goto(config.startingPage);
     // check we landed on the right page`
     await expect(page).toHaveTitle(/Home/, { timeout: 10000 });
     await expect(page.getByText('Gain increased visibility into your hybrid cloud')).toBeTruthy();
     await use(page);
+
+    if (testInfo.status == 'failed' || testInfo.status == 'timedOut') {
+      for (const logEntry of errLogs) {
+        console.error(logEntry);
+      }
+    }
   }
 });
