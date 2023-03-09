036b287261416a398a9a7253144d4043985bcb78
Jakub Stejskal <xstejs24@gmail.com>
2023-03-09 16:26:06 +0100

    Wait properly on home page content when we run the tests (#127)
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- lib/utils/fixtures.ts
+++ lib/utils/fixtures.ts
@@ -1,5 +1,5 @@
 import { config } from '@lib/config';
-import { test as base } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import blockAnalyticsDomains from '@lib/utils/blocker';
 
 // Extend Playright page to start always at config.startingPage
@@ -7,6 +7,9 @@ export const test = base.extend({
   page: async ({ page }, use) => {
     await blockAnalyticsDomains(page);
     await page.goto(config.startingPage);
+    // check we landed on the right page`
+    await expect(page).toHaveTitle(/Home/, { timeout: 10000 });
+    await expect(page.getByText('Gain increased visibility into your hybrid cloud')).toBeTruthy();
     await use(page);
   }
 });
