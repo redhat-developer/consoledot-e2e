3616e78a417519e93bfa6262b38ba9fe5c00fba5
Dominik Hlavac Duran <dhlavacd@redhat.com>
2023-03-09 10:02:16 +0100

    Add blocking of pendo and analytics scripts

--- lib/utils/blocker.ts
+++ lib/utils/blocker.ts
@@ -2,7 +2,7 @@ import { Page } from '@playwright/test';
 
 export default async function blockAnalyticsDomains(page: Page) {
   await page.route('**/*', (route) => {
-    const blockedUrls = ['trustarc', 'dpal.js', 'analytics', 'segment.com'];
+    const blockedUrls = ['trustarc', 'dpal.js', 'analytics', 'segment.com', 'pendo', 'AppMeasurement'];
     const url = route.request().url();
     return blockedUrls.some((blocked) => url.includes(blocked)) ? route.abort() : route.continue();
   });
